df93514cbc38723bb2920e9d114f31eb7ef1bfcc
Yash <yashraj504300@gmail.com>
2024-07-02 00:05:29 +0530

    postcss and nth-check vunerablities solved by upgrading version

--- luna/account/models.py
+++ luna/account/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 class CustomUser(models.Model):
     username = models.CharField(max_length=150)
+    
     password = models.CharField(max_length=128)
+    
     email = models.EmailField
